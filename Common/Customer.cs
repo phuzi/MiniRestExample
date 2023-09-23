fb95c62bb676b35104e377c6d774201685a46fb0
Brian Gentles <brian.gentles@gmail.com>
2023-09-23 22:36:49 +0100

    Add customer model

---
+++ Common/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace Common
+{
+    public class Customer
+    {
+        public Guid CustomerRef { get; set; } = Guid.Empty;
+
+        public string CustomerName { get; set; } = string.Empty;
+
+        public string AddressLine1 { get; set; } = string.Empty;
+     
+        public string AddressLine2 { get; set; } = string.Empty;
+
+        public string Town { get; set; } = string.Empty;
+        
+        public string County { get; set; } = string.Empty;
+        
+        public string Country { get; set; } = string.Empty;
+        
+        public string Postcode { get; set; } = string.Empty;
+    }
+}
