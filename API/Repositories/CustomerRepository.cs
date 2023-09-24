d362992ee86dd7305eba55e4ab9227b35d3518f0
Brian Gentles <brian.gentles@gmail.com>
2023-09-25 00:29:10 +0100

    Add and implement (I)CustomerRepository

---
+++ API/Repositories/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using API.DbContext;
+using Common;
+
+namespace API.Repositories;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly ICustomerDbContext _context;
+
+    public CustomerRepository(ICustomerDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<int> AddAsync(Customer customer, CancellationToken cancellationToken)
+    {
+        await _context.Customers!.AddAsync(customer, cancellationToken);
+        return await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async ValueTask<Customer?> GetAsync(Guid customerRef, CancellationToken cancellationToken)
+    {
+        return await _context.Customers!.FindAsync(new object[] { customerRef }, cancellationToken: cancellationToken);
+    }
+}
---
+++ API/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace API.Repositories;
+
+public interface ICustomerRepository
+{
+    Task<int> AddAsync(Customer customer, CancellationToken cancellationToken);
+
+    ValueTask<Customer?> GetAsync(Guid customerRef, CancellationToken cancellationToken);
+}
