8648e92902a6d3757aadda9afac4b3559d3e9ddb
Brian Gentles <brian.gentles@gmail.com>
2023-09-25 23:40:39 +0100

    Wire up AuthController

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using API.Services;
+using Azure.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -7,18 +9,28 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly IClientAuthenticationService _clientAuthenticationService;
         private readonly ILogger<AuthController> _logger;
 
-        public AuthController(ILogger<AuthController> logger)
+        public AuthController(IClientAuthenticationService clientAuthenticationService, ILogger<AuthController> logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _clientAuthenticationService = clientAuthenticationService ?? throw new ArgumentNullException(nameof(clientAuthenticationService));
         }
 
         [AllowAnonymous]
         [HttpGet]
-        public ActionResult Get([FromQuery] Guid clientId, [FromQuery] string clientID)
+        public ActionResult Get([FromQuery] string clientId, [FromQuery] string clientSecret)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var token = _clientAuthenticationService.Authenticate(clientId, clientSecret);
+                return Ok(new { token });
+            }
+            catch (AuthenticationFailedException ex)
+            {
+                return this.Unauthorized(ex.Message);
+            }
         }
     }
 }
