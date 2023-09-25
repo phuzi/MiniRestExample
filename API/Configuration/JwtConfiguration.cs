27bc50f1feab69da917f46d2c047252b07c8a335
Brian Gentles <brian.gentles@gmail.com>
2023-09-25 23:14:02 +0100

    Enable creation of JWT bearer tokens

---
+++ API/Configuration/JwtConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Configuration;
+
+public class JwtConfiguration
+{
+    public int Lifetime { get; set; } = 60;
+    public string? Issuer { get; set; }
+    public string? Audience { get; set; }
+    public string? Key { get; set; }
+}
---
+++ API/Services/ClientAuthenticationService.cs
@@ -0,0 +1,46 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Configuration;
+using Azure.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class ClientAuthenticationService : IClientAuthenticationService
+{
+    private readonly JwtConfiguration _configuration;
+    private readonly ApiCredentials _apiCredentials;
+
+    public ClientAuthenticationService(IOptions<JwtConfiguration> jwtConfiguration, IOptions<ApiCredentials> credentials)
+    {
+        _configuration = jwtConfiguration.Value;
+        _apiCredentials = credentials.Value;
+    }
+
+    public string Authenticate(string clientId, string clientSecret)
+    {
+        if (clientId != _apiCredentials.ClientId || clientSecret != _apiCredentials.ClientSecret)
+        {
+            throw new AuthenticationFailedException("ClientId and Secret are invalid");
+        }
+
+        var claims = new[] { new Claim("sub", clientId) };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Key ?? string.Empty));
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration.Issuer ?? string.Empty,
+            audience: _configuration.Audience ?? string.Empty,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(_configuration.Lifetime),
+            signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256));
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
+
+public interface IClientAuthenticationService
+{
+    string Authenticate(string clientId, string clientSecret);
+}
